6a7f4816c2da7f17d641ae2e2d2e361ff1021826
BG <BG@HIMA>
2024-12-03 20:05:26 +0200

    Add project files.

---
+++ Inventory.Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using Inventory.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public OrderType OrderType { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal ItemDiscount { get; set; }
+        public decimal Tax { get; set; }
+        public decimal ShippingCharges { get; set; }
+        public decimal Total { get; set; }
+        public string PromoCode { get; set; }
+        public decimal Discount { get; set; }
+        public decimal GrandTotal { get; set; }
+        public string Description { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
